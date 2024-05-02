947498e5c881a0b31382ac7d0ebe785018dc2a45
Alberto Arias <alberto.arias.x64@gmail.com>
2024-05-02 15:11:18 -0500

    first try

---
+++ app.js
@@ -0,0 +1,20 @@
+import playwright from 'playwright'
+import fs from 'fs/promises'
+
+const data = (await fs.readFile('./scraper.conf')).toString()
+const PAGE = new RegExp(/PAGE=(.*)/gm).exec(data)[1]
+const ITEM = new RegExp(/ITEM=(.*)/gm).exec(data)[1]
+const CLASS_ELEMENT = new RegExp(/CLASS_ELEMENT=(.*)/gm).exec(data)[1]
+
+async function agent() {
+  const browser = await playwright.chromium.launch()
+  const context = await browser.newContext()
+  const page = await context.newPage()
+  await page.goto(PAGE + ITEM)
+  const values = await page.getAttribute(`.${CLASS_ELEMENT}`)
+  await page.waitForTimeout(1000)
+  await browser.close()
+  console.log(values)
+}
+
+agent()
---
+++ readme.md
@@ -0,0 +1,24 @@
+# Web Scraper con Playwright
+## Descripción del proyecto:
+
+Este proyecto utiliza la biblioteca Playwright para scrapear datos de una página web. Playwright es una herramienta de automatización web que permite controlar navegadores web como Chrome, Firefox y Chromium.
+
+## Requisitos:
+
+- Node.js
+- Playwright
+
+## DEV
+
+### Clonar el repositorio
+``` bash
+git clone https://github.com/Alberto-Arias-x64/web-scraper.git
+```
+
+### Instalar depencias:
+
+``` bash
+npm install
+npx playwright install
+```
+
