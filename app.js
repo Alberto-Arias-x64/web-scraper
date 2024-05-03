34b3aa0dbe56baa094ac228f1ef95dd3ec83dab4
Alberto Arias <alberto.arias.x64@gmail.com>
2024-05-03 10:24:42 -0500

    scarper v2

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
+// @ts-check
+
 import playwright from 'playwright'
 import fs from 'fs/promises'
 
 const data = (await fs.readFile('./scraper.conf')).toString()
-const PAGE = new RegExp(/PAGE=(.*)/gm).exec(data)[1]
-const ITEM = new RegExp(/ITEM=(.*)/gm).exec(data)[1]
-const CLASS_ELEMENT = new RegExp(/CLASS_ELEMENT=(.*)/gm).exec(data)[1]
+const PAGE = new RegExp(/PAGE=(.*)/gm).exec(data)?.[1] ?? ''
+const ITEM = new RegExp(/ITEM=(.*)/gm).exec(data)?.[1] ?? ''
+const CLASS_ELEMENT = new RegExp(/CLASS_ELEMENT=(.*)/gm).exec(data)?.[1] ?? ''
 
 async function agent() {
   const browser = await playwright.chromium.launch()
   const context = await browser.newContext()
   const page = await context.newPage()
   await page.goto(PAGE + ITEM)
-  const values = await page.getAttribute(`.${CLASS_ELEMENT}`)
+  const values = await page.textContent(`[class="${CLASS_ELEMENT}"]`)
   await page.waitForTimeout(1000)
   await browser.close()
   console.log(values)
